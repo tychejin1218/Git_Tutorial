0734086acc62cfa58b7dd389d0148088cc4efdb9
tychejin1218 <tychejin@naver.com>
2020-10-10 10:46:15 +0900

    branch : develop Init Commit

--- Git_Tutorial/src/tutorial/GitTutorial.java
+++ Git_Tutorial/src/tutorial/GitTutorial.java
@@ -3,6 +3,9 @@ package tutorial;
 public class GitTutorial {
 
 	public static void main(String[] args){
+		
 		System.out.println("Git Tutorial Init");
+		
+		System.out.println("branch - develop");
 	}
 }
